b72d8d94f5dd8699175e4bf454e12ce68786edb7
rendraar <rendrarizqi611@gmail.com>
2024-11-18 13:34:57 +0700

    added files for control request to database

---
+++ codelab/app/Config/DatabaseConfig.php
@@ -0,0 +1,11 @@
+<?php
+// File: C:\xampp\htdocs\app\Config\DatabaseConfig.php
+namespace app\Config;
+
+class DatabaseConfig {
+    protected $host = 'localhost';
+    protected $user = 'root';
+    protected $password = '';
+    protected $database_name = 'web_codelab4';
+    protected $port = 3306;
+}
